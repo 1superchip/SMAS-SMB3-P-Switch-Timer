d200bc68a8a6e2183c6c41dd0a048a426d64f78e
1superchip <57328807+1superchip@users.noreply.github.com>
2022-02-26 14:26:43 -0600

    Format source

--- PatchSource.asm
+++ PatchSource.asm
@@ -1,8 +1,9 @@
 
+; SMAS SMB3 P-Switch Timer
+
 ; Source for Super Mario All Stars SMB3 P-Switch Timer Display
 ; Created by 1superchip
 ; All of thise code is injected into bank 0x29
-
 
 ORG $29E716
 UpdateInstruction:
@@ -12,10 +13,8 @@ ORG $29E71A
 AddNop:
 nop
 
-
 ORG $29C1EC
 PSwitch_Hijack:
-
 LDX $0567 ;load p-switch timer
 CPX #$00
 BEQ end ;if 0 branch end
@@ -31,7 +30,6 @@ SEC
 SBC #$0A
 INY
 JMP modulusLoop
-
 ;end of loop
 
 end:
@@ -40,8 +38,5 @@ ORA #$90 ;default instruction (needed)
 rtl
 
 ORG $29FF84
-
-
-
 
 
